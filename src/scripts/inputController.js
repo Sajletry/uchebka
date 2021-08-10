f5bfa479a4841c8898cb81687e483467d8cc39e8
Vasily Vorochaev <v.vorochaev@artox.com>
2021-08-10 22:17:25 +0300

    lesson-3-end

--- src/scripts/inputController.js
+++ src/scripts/inputController.js
@@ -1,25 +1,40 @@
-const greenColor = '#009900';
-const blueColor = '#000099';
-
-function changeColor(input){
-  if(input.dataset.isgreen === "true"){
-    input.style.background = blueColor;
-    input.dataset.isgreen = "false";
-  }else if(input.dataset.isgreen === "false"){
-    input.style.background = greenColor;
-    input.dataset.isgreen = "true";
-  }
-}
-
-function onTimer(input){
-  setInterval(()=>{
-    changeColor(input);
-  },1000);
-}
 
 window.addEventListener('DOMContentLoaded', (event) => {
-  const login = document.getElementById('login');
-  const password = document.getElementById('password');
-  onTimer(login);
-  onTimer(password);
+  const ball = document.getElementById('ball');
+  const left = document.getElementById('left');
+  const right = document.getElementById('right');
+  let isDrugged;
+  let position = 'left';
+
+  function drugStart(){
+    isDrugged = true;
+    ball.style.position = 'absolute';
+  }
+
+  function drugmove(event){
+    if(isDrugged){
+    ball.style.top = event.pageY+'px';
+    ball.style.left = event.pageX+'px';
+    }
+  }
+
+  function drugStop(){
+    if(isDrugged) {
+      isDrugged = false;
+      if(position==='left'){
+        left.appendChild(ball)
+      }
+      if(position==='right'){
+        right.appendChild(ball)
+      }
+    }
+  }
+
+
+
+  ball.addEventListener('mousedown',drugStart);
+  document.body.addEventListener('mousemove',drugmove)
+  document.body.addEventListener('mouseup',drugStop)
+  left.addEventListener("mouseenter", ()=>{position="left"})
+  right.addEventListener("mouseenter", ()=>{position="right"})
 });
